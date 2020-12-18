c5863a4ca3fb50289e2b97a6fff54dbf294db12f
ahinsyar <ahinsyar@gmail.com>
2020-12-18 18:36:49 +0900

    테스트 코드 추가 및 활용 방법 코드 리펙토링

--- inflearn/express/index.js
+++ inflearn/express/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const user = require('./api/user')
 const app = express();
 
-var users = [
-    {id:1, name:'alice'},
-    {id:2, name:'bek'},
-    {id:3, name:'cris'},
-]
+// var users = [
+//     {id:1, name:'alice'},
+//     {id:2, name:'bek'},
+//     {id:3, name:'cris'},
+// ]
 
 
 // function commonmw(req, res, next){
@@ -37,14 +39,75 @@ var users = [
 // app.use(morgan('dev'))
 
 app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use('/users', user)
 
-app.get('/users', function(req, res){
-    res.json(users);
-})
-app.post('users', function(req, res){
-    //create user....
-})
+// app.get('/users', function(req, res){
+//     req.query.limit = req.query.limit || 10
+//     const limit = parseInt(req.query.limit, 10)
+//     if(Number.isNaN(limit)){
+//         return res.status(400).end();
+//     }
+//     res.json(users.slice(0, limit));
+// })
+// app.get('/users/:id', function(req, res){
+//     const id = parseInt(req.params.id, 10);
+//     if(Number.isNaN(id)){
+//         return res.status(400).end();
+//     }
+//     const user = users.filter((user) =>
+//         user.id === id
+//     )[0];
+//     if(!user){
+//         return res.status(404).end();
+//     }
+//     res.json(user);
+// })
+// app.delete('/users/:id', function(req, res) {
+//     const id = parseInt(req.params.id, 10)
+//     if(Number.isNaN(id)){
+//         return res.status(400).end();
+//     }
+//     users = users.filter(user => user.id !== id);
+//     res.status(204).end()
+// })
+
+// app.post('/users', function(req, res){
+//     const name = req.body.name
+//     if(!name){return res.status(400).end()}
+//     const isConflict = users.filter(user => user.name === name).length
+//     if(isConflict){ return res.status(409).end(); }
+//     const id = Date.now();
+//     const user = {id, name}
+//     users.push(user)
+//     res.status(201).json(user);
+// })
+
+// app.put('/users/:id', (req, res)=>{
+//     const id = parseInt(req.params.id, 10);
+//     if(Number.isNaN(id)){
+//         return res.status(400).end()
+//     }
+//     const name = req.body.name;
+//     if(!name){
+//         return res.status(400).end()
+//     }
+//     const isConflict = users.filter(user => user.name === name).length
+//     if(isConflict){
+//         return res.status(409).end();
+//     }
+
+//     const user = users.filter(user=> user.id === id)[0];
+//     if(!user){
+//         return res.status(404).end();
+//     }
+//     user.name = name;
+//     res.json(user);
+// })
 
 app.listen(3000, function(){
     console.log('server is running')
 })
+
+module.exports = app;
